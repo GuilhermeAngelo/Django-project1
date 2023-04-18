59aca92853669d756adc28b569c1379587e48819
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2023-04-18 15:48:33 -0300

    criando a página home

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/pages/home.html')
 
 
 def about(request):
